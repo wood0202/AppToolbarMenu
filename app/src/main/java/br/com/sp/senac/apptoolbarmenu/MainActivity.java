06f2238da1cf87060d1d524a0adb9788b1f632dc
lucas.aoliveira34 <lucas.oliveira01@hotmail.com>
2020-03-10 11:50:53 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/apptoolbarmenu/MainActivity.java
@@ -0,0 +1,73 @@
+package br.com.sp.senac.apptoolbarmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //variavel global
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //apresentação do objeto java para objeto layout
+
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    //
+    //O menu fui Inflado
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mSalvar:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no Salvar!!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mAlterar:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no Alterar!!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mExcluir:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no Excluir!!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mBuscar:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no Buscar!!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mSair:
+                Toast.makeText(getApplicationContext(),
+                        "Clique no Sair!!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
